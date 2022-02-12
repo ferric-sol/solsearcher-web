ad60cfe732a8133fcbc074e827ef15879d439d2d
ferric <ferric-sol@ferric.net>
2022-02-12 18:30:13 -0500

    Use a diff endpoint for now

--- src/views/HomeView/index.tsx
+++ src/views/HomeView/index.tsx
@@ -20,7 +20,7 @@ export const HomeView: FC = ({}) => {
   const [searchTerm, setSearchTerm] = useState<any>();
   const { publicKey, signMessage } = useWallet();
 
-  const url = "http://api.solsearcher.space/all_collections"
+  const url = "https://api.solsearcher.space/all_collections"
   const { data } = useSWR(
     url,
     fetcher,
