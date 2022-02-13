64c117f9481e0dead740359035bf075bdcc672c1
ferric <ferric-sol@ferric.net>
2022-02-12 21:53:24 -0500

    Lets try with cors-anywhere

--- src/views/HomeView/NftCard.tsx
+++ src/views/HomeView/NftCard.tsx
@@ -26,7 +26,7 @@ export const NftCard: FC<Props> = ({
 
   console.log('details: ', details)
 
-  const uri = "https://api-mainnet.magiceden.io/rpc/getCollectionEscrowStats/"+details?.symbol;
+  const uri = "https://stark-lake-75007.herokuapp.com:8080/https://api-mainnet.magiceden.io/rpc/getCollectionEscrowStats/"+details?.symbol;
 
   const { data, error } = useSWR(
     uri,
